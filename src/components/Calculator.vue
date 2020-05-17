ff899a2d84a828188c194acd9c079c7133472895
Maeva Wolff <maeva.wolff@hetic.net>
2020-05-17 17:30:40 +0200

    add functions to use calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,42 +1,96 @@
-
 <template>
   <div>
     <h1>Calculator</h1>
     <div class="calculator">
       <div class="current">{{ current || 0 }}</div>
 
-      <div class="btn">AC</div>
-      <div class="btn">+/-</div>
-      <div class="btn">%</div>
-      <div class="btn">/</div>
+      <div @click="clear()" class="btn">AC</div>
+      <div @click="sign()" class="btn">+/-</div>
+      <div @click="percent()" class="btn">%</div>
+      <div @click="divide()" class="btn operator">/</div>
 
-      <div class="btn">7</div>
-      <div class="btn">8</div>
-      <div class="btn">9</div>
-      <div class="btn">x</div>
+      <div @click="append('7')" class="btn">7</div>
+      <div @click="append('8')" class="btn">8</div>
+      <div @click="append('9')" class="btn">9</div>
+      <div @click="times()" class="btn operator">x</div>
 
-      <div class="btn">4</div>
-      <div class="btn">5</div>
-      <div class="btn">6</div>
-      <div class="btn">-</div>
+      <div @click="append('4')" class="btn">4</div>
+      <div @click="append('5')" class="btn">5</div>
+      <div @click="append('6')" class="btn">6</div>
+      <div @click="minus()" class="btn operator">-</div>
 
-      <div class="btn">1</div>
-      <div class="btn">2</div>
-      <div class="btn">3</div>
-      <div class="btn">+</div>
+      <div @click="append('1')" class="btn">1</div>
+      <div @click="append('2')" class="btn">2</div>
+      <div @click="append('3')" class="btn">3</div>
+      <div @click="add()" class="btn operator">+</div>
 
-      <div class="btn zero">0</div>
-      <div class="btn">,</div>
-      <div class="btn">=</div>
+      <div @click="append('0')" class="btn zero">0</div>
+      <div @click="dot()" class="btn">,</div>
+      <div @click="equal()" class="btn operator">=</div>
     </div>
   </div>
 </template>
 
-<script>
+<script> 
 export default {
+  /* eslint-disable */
   data() {
     return {
-      current: ''
+      previous: null,
+      current: '',
+      operator: null,
+      operatorClicked: false
+    }
+  },
+  methods: {
+    clear() {
+      this.current = ''
+    },
+    sign() {
+      this.current = this.current.charAt(0) === '-'
+        ? this.current.slice(1) : `-${this.current}`
+    },
+    percent() {
+      this.current = `${parseFloat(this.current) / 100}`
+    },
+    append(number) {
+      if (this.operatorClicked) {
+        this.current = ''
+        this.operatorClicked = false
+      }
+      this.current = `${this.current}${number}`
+    },
+    dot() {
+      if (this.current.indexOf('.') === -1) {
+        this.append('.')
+      }
+    },
+    setPrevious() {
+      this.previous = this.current
+      this.operatorClicked = true
+    },
+    divide() {
+      this.operator = (a, b) => a / b
+      this.setPrevious()
+    },
+    times() {
+      this.operator = (a, b) => a * b
+      this.setPrevious()
+    },
+    minus() {
+      this.operator = (a, b) => a - b
+      this.setPrevious()
+    },
+    add() {
+      this.operator = (a, b) => a + b
+      this.setPrevious()
+    },
+    equal() {
+      this.current = `${this.operator(
+        parseFloat(this.current),
+        parseFloat(this.previous)
+      )}`
+      this.previous = null
     }
   }
 }
@@ -79,5 +133,14 @@ export default {
 
 .zero {
   grid-column: 1/3;
+}
+
+.operator {
+  background-color: orangered;
+  color: #f2f2f2;
+}
+
+.operator:hover {
+  background-color: rgb(255, 115, 0);
 }
 </style>
